6d7ef58327aea9fde4e8e4c20852d757f7abd536
unknown <sudeepkudari0@gmail.com>
2024-04-24 16:30:18 +0530

    uploads

--- tru-storefront/next.config.js
+++ tru-storefront/next.config.js
@@ -28,6 +28,10 @@ const nextConfig = withStoreConfig({
       {
         protocol: "https",
         hostname: "drive.google.com",
+      },
+      {
+        protocol: "https",
+        hostname: "https://sea-turtle-app-s9bl2.ondigitalocean.app"
       }
     ],
   },
